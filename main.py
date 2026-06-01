cc5c85438fda8c8ccc766562e010ade6d17cd453
RubenSab <rubensabatini2005@gmail.com>
2026-06-01 17:49:37 +0200

    fixed romanian

--- main.py
+++ main.py
@@ -11,4 +11,4 @@ if __name__=='__main__':
     print(f"in english, {n} is {digits_to_words(n, eng_rules)}")
     print(f"in italiano, {n} è {digits_to_words(n, ita_rules)}")
     print(f"en español, {n} es {digits_to_words(n, esp_rules)}")
-    print(f"ande romani chib {n} es {digits_to_words(n, ron_rules)}")
+    print(f"în limba română, {n} este {digits_to_words(n, ron_rules)}")
